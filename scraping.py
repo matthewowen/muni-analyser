507b26180255f6f7a2e41f7a505171e257f8a054
Matthew Owen <matthew.owen95@gmail.com>
2012-10-23 12:48:38 +0100

    initial commit

---
+++ scraping.py
@@ -0,0 +1,72 @@
+from BeautifulSoup import BeautifulStoneSoup
+import sqlite3, requests, time, multiprocessing, boto
+from settings import AWS_KEY, AWS_SECRET
+from urllib import quote
+
+DATABASE = 'db'
+
+
+def get_content(URL):
+    """
+    Pass a URL. Get back some Beautiful Soup.
+    """
+
+    req = requests.get(URL, timeout=15)
+    soup = BeautifulStoneSoup(req.content, selfClosingTags='stop')
+    return soup
+
+
+def run_cycle():
+    """
+    Run the lovely cycle of getting times
+    """
+
+    def do_stop(stop):
+        url = "http://webservices.nextbus.com/service/publicXMLFeed?" \
+              "command=predictions&a=sf-muni&r=%s&s=%s" % (stop[1], stop[0])
+
+        soup = get_content(url)
+
+        time_now = time.time()
+
+        vehicles = soup.findAll('prediction')
+
+        for vehicle in vehicles:
+            data = {}
+            data['trip'] = vehicle['triptag']
+            data['time'] = time_now
+            data['stop'] = stop[0]
+            data['route'] = stop[1]
+            data['togo'] = int(vehicle['seconds'])
+            item_name = '%s:%s:%s' % (data['time'], data['stop'], data['trip'])
+
+            dom.put_attributes(item_name, data)
+
+    conn = sqlite3.connect(DATABASE)
+    c = conn.cursor()
+
+    c.execute('SELECT stop_id, route_tag from routestops where rowid % 4 == 0')
+
+    r = c.fetchall()
+
+    conn = boto.connect_sdb(aws_access_key_id=AWS_KEY,
+                            aws_secret_access_key=AWS_SECRET)
+    dom = conn.get_domain('predictions')
+
+    started = time.time()
+
+    #manager = multiprocessing.Manager()
+    #datapoints = manager.list()
+    processes = []
+    for stop in r:
+        p = multiprocessing.Process(target=do_stop, args=(stop,))
+        p.start()
+        processes.append(p)
+
+    for process in processes:
+        process.join()
+
+
+# set this up in cron so that this script runs periodically
+
+run_cycle()
